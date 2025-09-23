f83b7b83054c3b4d782087418385c1e5579c7e52
rothmail <arpiovesana@gmail.com>
2025-09-23 15:43:21 -0300

    teste

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+let toggleNavStatus = false;
+
+let toggleNav = function () {
+    let getSidebar = document.querySelector('.nav-sidebar');
+    let getSidebarLinks = document.querySelectorAll('.nav-sidebar a');
+    let logoBtnMenu = document.querySelector('.btn-toggle-nav');
+
+    if (toggleNavStatus === false) {
+        getSidebar.style.width = "250px";
+        getSidebar.style.visibility = "visible";
+
+        getSidebarLinks.forEach(link => {
+            link.style.opacity = "1";
+        });
+
+        logoBtnMenu.style.backgroundColor = "var(--fundo-bege)";
+        toggleNavStatus = true;
+    } else {
+        getSidebar.style.width = "0";
+        getSidebar.style.visibility = "hidden";
+
+        getSidebarLinks.forEach(link => {
+            link.style.opacity = "0";
+        });
+
+        logoBtnMenu.style.backgroundColor = "var(--fundo-bege)";
+        toggleNavStatus = false;
+    }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    let menuLinks = document.querySelectorAll(".nav-sidebar a");
+    menuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            if (toggleNavStatus) {
+                toggleNav();
+            }
+        });
+    });
+});
+
+function navegarListaEquipes() {
+    window.location.href = "lista-equipes.html";
+}
