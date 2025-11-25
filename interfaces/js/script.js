be2662f6b150cdda209415e9cd70ebd6f6c75632
rothmail <arpiovesana@gmail.com>
2025-11-25 15:07:59 -0300

    login e cadastro - frontend

--- interfaces/js/script.js
+++ interfaces/js/script.js
@@ -146,3 +146,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+// LOGIN E CADASTRO
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
